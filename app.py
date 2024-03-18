069cc413037766b611c88e3edd59f4fa74009bab
Bernabe Suarez <99522438+BernabeSuarez@users.noreply.github.com>
2024-03-18 15:20:03 -0300

    s

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#importacion de librerias
+#importacion de libreriass
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL,MySQLdb
 import re
